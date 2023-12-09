f8727ca42f11c6b5f48858cdd76918f7cde4975a
Fran Sanchez <fran@mipixel.com>
2023-12-09 02:23:17 +0000

    Ignore unnimplemented test

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -119,6 +119,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(30));
